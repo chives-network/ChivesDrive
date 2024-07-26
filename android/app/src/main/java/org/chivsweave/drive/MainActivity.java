51dcadc46ac5ca0755cd0c8294db9cc44bd37f34
Chives Network <chivesreact@gmail.com>
2024-07-26 10:01:15 +0800

    Capacitor

---
+++ android/app/src/main/java/org/chivsweave/drive/MainActivity.java
@@ -0,0 +1,5 @@
+package org.chivsweave.drive;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'org.chivsweave.drive',
+  appName: 'chivesdrive',
+  webDir: 'out'
+};
+
+export default config;
