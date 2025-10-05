7073f79024a9fd800bb83f44f384f3df70400195
Chives-network <reactchives@gmail.com>
2025-10-06 08:51:28 +0900

    Address Name List

--- src/configs/addressname.ts
+++ src/configs/addressname.ts
@@ -1,10 +1,8 @@
 export default {
-    'W9BpPw2sBGM-5P36WmSQONWozwgR_Ceand0rLgWc4k8': 'Undecided purpose Wallet 1',
-    'ZlVdBorekvAPXeFotmzvfaM34WdpL8KA6G7n9J2chJA': 'Undecided purpose Wallet 2',
-    'Z3GlNxIKHa-yLt2-0NxkgfIp-DKgOqGTRiT8M16AAr0': 'Dev & Service Team Funding',
-    'WXuUWhzLqDJ1A2zXGxCKty1rvSOxq9avAZQGsIvRojs': 'Ecosystem Project Funding',
-    'WqEab7Df5yrKP93ikffZMplCrFefZl0zWyGX5DyGiOw': 'Arweave User Airdrop',
-    'ZbWbvPzcFu3znB1Q-4sflIloVuNmhdxkgI1MgqPqWuk': 'Chives Light Node Reward Pool',
-    'xwA8HpOT9BI0iSKRDYTWhM7awHV6Xi_iTmtnGrAm4Xk': 'Chives Light Node Reward Send',
-    'Ei-psc7SkN7kLsUNMLCUxA-Tk6AhtEDm90-czY20pbQ': 'Faucet Wallet'
+    'AGHUXnGPCwBZ5zyoBc_CGjKLWHp6dhXkr-WVq6gdIXY': 'Undecided purpose Wallet 1',
+    'k8P09SOzK_9l2As6iJrMETkK1ktyh1OzL5-OtOAuJFw': 'Undecided purpose Wallet 2',
+    'biAAPhtFjLg0a8tsevd0rkmlRmby5vPNnJzldvIh3h0': 'Dev & Service Team Funding',
+    '1kh0izmUC0ORzvduv3rnhwHduwbmlm2ptDAYzUpNUTw': 'Ecosystem Project Funding',
+    'ufUWsjH6xa8D9E6K96TaQr7OEfkCtsxnMDJW2WeqbFg': 'Arweave User Airdrop',
+    'UV0RpwR0ArMF1Ki1q6kbneDNeren6veqW_85W9KA_Ek': 'Chives Reward Pool'
 }
